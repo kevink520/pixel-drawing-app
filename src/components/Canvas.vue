baca3023e52ff035406469fa55a9e558cd42796a
Kevin Kim <kevinkim75@gmail.com>
2020-08-11 02:30:20 -0400

    Completed the app

---
+++ src/components/Canvas.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="canvas">
+    <Pixel isInCanvas v-for="(color, index) in pixels" :key="index" :index="index" :color="color" />
+  </div>
+</template>
+
+<script>
+  import Pixel from './Pixel';
+
+  export default {
+    name: 'Canvas',
+    components: {
+      Pixel
+    },
+    props: {
+      pixels: Array,
+      color: String
+    },
+    mounted() {
+      this.$root.$on('clickedpixel', index => {
+        this.pixels.splice(index, 1, this.color);
+      });
+    }
+  }
+</script>
+
+<style>
+  .canvas {
+    display: flex;
+    flex-wrap: wrap;
+    max-width: 900px;
+  }
+</style>
---
+++ src/components/ColorPicker.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <Pixel isInColorPicker color="white" :current="color === 'white'" />
+    <Pixel isInColorPicker color="lightblue" :current="color === 'lightblue'" />
+    <Pixel isInColorPicker color="blue" :current="color === 'blue'" />
+    <Pixel isInColorPicker color="darkblue" :current="color === 'darkblue'" />
+  </div>
+</template>
+
+<script>
+import Pixel from './Pixel';
+
+export default {
+  name: 'ColorPicker',
+  props: {
+    color: String
+  },
+  components: {
+    Pixel
+  }
+}
+</script>
